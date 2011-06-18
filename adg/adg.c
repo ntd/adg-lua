b5863dc8a0ca701d595b197cffc6edb56fc73b56
Nicola Fontana <ntd@entidi.it>
2011-06-18 18:11:48 +0200

    Updated copyright docblocks
    The project is now separated from lgob: make it clear in the copyright
    docblocks. Furthermore, the previous docblocks said the license is LGPL
    3 so the COPYING file has been updated accordingly.

--- adg/adg.c
+++ adg/adg.c
@@ -1,20 +1,18 @@
-/*
- * This file needs lgob although it is not officialy part of it.
+/* adg-lua - lgob based Lua bindings for the ADG canvas
+ * Copyright (C) 2011  Nicola Fontana <ntd at entidi.it>
  *
- * lgob is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * adg-lua is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
- * lgob is distributed in the hope that it will be useful,
+ * adg-lua is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with lgob.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2011  Nicola Fontana
+ * along with adg-lua.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <lua.h>
