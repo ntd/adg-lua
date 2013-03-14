2b8a3783bfcb162d0ff1b56eaa382abfd1d43252
Nicola Fontana <ntd@entidi.it>
2013-03-14 16:17:30 +0100

    build: updated build system
    Actually the adg-lua project is only an example application on using the
    LGI bindings with ADG. In other words, ADG and LGI works together out of
    the box.

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+step() {
+    local message="$1"
+    local command="$2"
 
-if test ! -f $srcdir/configure.ac -o ! -f $srcdir/examples/Adg.lua; then
-    echo "**Error**: '$srcdir' does not look like the top-level adg-lua directory"
-    exit 1
-fi
+    printf "$message... "
 
-cd $srcdir
-libtoolize --automake || exit $?
-autoreconf -is -Wall || exit $?
-./configure "$@" && echo "Now type 'make' to compile $PROJECT."
+    if eval $command >/dev/null 2>&1; then
+	printf "\033[32mok\033[0m\n"
+    else
+	local result=$?
+	printf "\033[31mfailed\033[0m\n  ** \"$command\" returned $result\n"
+	exit $result
+    fi
+}
+
+
+dir=`dirname $0`
+test -z "$dir" && dir=.
+
+step	"Checking for top-level adg-lua directory" \
+	"test -f '$dir/adg-demo.lua'"
+
+step	"Creating dummy ChangeLog, if needed" \
+	"test -f '$dir/ChangeLog' || touch '$dir/ChangeLog'"
+
+step	"Regenerating autotools files" \
+	"autoreconf -is -Wall '$dir'"
+
+
+printf "Now run \033[1m$dir/configure\033[0m to customize your building\n"
